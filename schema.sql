2d4ddd6460c1a3401f0d3c623ba4621c24c92427
matovu-farid <matovu90@gmail.com>
2022-03-22 23:58:05 +0300

    Modify schema

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name text, date_of_birth date, escape_attempts integer,  neutered boolean, weight_kg decimal);
+ALTER TABLE animals ADD species text;
