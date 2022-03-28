77eb666f05ecb9a24bfb47593636e524b08816d0
matovu-farid <matovu90@gmail.com>
2022-03-28 17:34:35 +0300

    Select from big data added

--- queries.sql
+++ queries.sql
@@ -149,6 +149,15 @@ GROUP BY  a.name
 ORDER BY COUNT(a.name) DESC
 LIMIT 1;
 
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
+
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+
 
 
 
